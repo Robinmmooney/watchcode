b0ee469e10985965c4805cf9c6306eb51da52f19
Robin Mooney <robinmooney.student@careerdevs.com>
2019-01-31 17:31:47 +0000

    testing git code

---
+++ todos.js
@@ -0,0 +1,17 @@
+// create an array named todos
+var todos = ["item 1", "item 2", "item 3", "item 4"];
+
+// console log it
+console.log('My todos:' , todos);
+
+//add to an array
+todos.push('item 4');
+
+//change an array
+todos[0] = 'item 1 updated';
+
+//delete an item from an array
+todos.splice(0,1);
+
+//testing git this code works
+
